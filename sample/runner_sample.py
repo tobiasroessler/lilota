ca5a5dd04af789d117f2ddf18da80432dc8b634d
Tobias Rössler <tobiasroessler@gmail.com>
2024-05-04 23:18:15 +0200

    runner_sample.py added

---
+++ sample/runner_sample.py
@@ -0,0 +1,28 @@
+import sys
+import time
+sys.path.append("../lilota")
+
+from lilota.runner import TaskRunner
+from lilota.models import TaskBase
+from lilota.stores import MemoryTaskStore, StoreManager
+
+class AddTask(TaskBase):
+
+  def run(self):
+    self.task_info.output = {
+      "result": self.task_info.input["number1"] + self.task_info.input["number2"]
+    }
+    print(self.output)
+    
+
+if __name__ == "__main__":
+  StoreManager.register("Store", MemoryTaskStore)
+  store_manager = StoreManager()
+  store_manager.start()
+  store = store_manager.Store()
+  runner = TaskRunner(store)
+  runner.register("add_task", AddTask)
+  runner.start()
+  runner.add("addition_task", "This task will add number1 to number2 and outputs the result", {"number1": 1, "number2": 2})
+  runner.stop()
+  time.sleep(30)
