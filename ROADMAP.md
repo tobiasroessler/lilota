bfa46810871f06bc4a23a16a12917846589603b7
Tobias Roessler <tobiasroessler@gmail.com>
2026-05-01 23:25:46 +0200

    Cleanup and version 1.0.0 created

--- ROADMAP.md
+++
@@ -1,6 +0,0 @@
-# TODO
-
-
-## Version 1.1.0
-
-* Add a logger as an additional parameter
--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from setuptools import setup, find_packages
 
 setup(
     name="lilota",
-    version="0.25.0",
+    version="1.0.0",
     packages=find_packages(include=["lilota"]),
 )
